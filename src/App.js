e00f740ce3efee704e23875883addf7b8d1181c0
Frank <frank.filius@incentro.com>
2020-02-25 13:11:42 +0100

    Update components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import HomePage from './homepage.component'
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage.component";
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/hats" component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }

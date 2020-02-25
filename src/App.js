493b2208c88d1c31436143c2d341fa13a5c113ac
Frank <frank.filius@incentro.com>
2020-02-25 14:16:23 +0100

    Move homepage to own folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
 

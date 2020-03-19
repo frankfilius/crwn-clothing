84d75175181a005b9e6aaa192eedc624e00609e0
Frank <frank.filius@incentro.com>
2020-03-19 11:31:43 +0100

    Create shop-reducer, Separate shop-overview to its own component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,11 @@
-import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import React from 'react';
 
-class ShopPage extends React.Component {
-  constructor() {
-    super();
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview
-            key={id}
-            {...otherCollectionProps}
-          ></CollectionPreview>
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
